f2111bfa676e1a5f6f1c13f86ae2a28ffa624d37
jatinsahijwani <jatinsahijwani2151@gmail.com>
2025-06-14 18:48:18 +0530

    test functionality implemented

--- bin/cli.js
+++ bin/cli.js
@@ -2,12 +2,20 @@
 
 const { program } = require('commander');
 const { compileCircuit } = require('../lib/compile');
+const { testCircuit } = require('../lib/test');
 
 program
   .command('compile <circomFilePath>')
   .description('Compile a circom circuit')
   .action((circomFilePath) => {
     compileCircuit(circomFilePath);
+  });
+
+program
+  .command('test <folder> <inputJson>')
+  .description('Test the circuit with input.json and generate proof/public.json')
+  .action((folder, inputJson) => {
+    testCircuit(folder, inputJson);
   });
 
   program.parse(process.argv);
